f301faa30070b5c61cf440f9cbd2db8491b50282
finencoarse <154966371+finencoarse@users.noreply.github.com>
2023-12-27 16:15:48 +0800

    Add files via upload

---
+++ ouob.py
@@ -0,0 +1,15 @@
+tiredness=input("are you tired?")
+if tiredness =='yes':
+    effort=input("did you work hard?")
+    if effort =='yes':
+       print("Let\'s take some rest!")
+    else:
+        print("Why did you waste your time on others things instead of working hard?")
+else:
+    status=input("did you finish anything?")
+    if status == 'yes':
+        print("Keep on the good work!")
+    else:
+        print("Why would you chose to blank out?")
+
+
